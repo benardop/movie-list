f299555e3300ca4ecbe7b89297b2521902415eb2
benardop <benard.oc.p@gmail.com>
2023-03-22 23:35:12 +0300

    Add application database access cridentials

--- src/main/java/com/benard/movies/MoviesApplication.java
+++ src/main/java/com/benard/movies/MoviesApplication.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String apiRoot() {
-		return "Hello, World!";
 	}
 
 }
