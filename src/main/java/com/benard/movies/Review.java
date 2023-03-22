68e0123fdeab4a4f3c46fbbf38a02fcc7fa07d9d
benardop <benard.oc.p@gmail.com>
2023-03-22 23:33:26 +0300

    Add movies review class

--- src/main/java/com/benard/movies/Review.java
+++ src/main/java/com/benard/movies/Review.java
@@ -1,4 +1,18 @@
 package com.benard.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
 }
