44e48e6e7bf77a98eac31c139fa4410277bf4c8b
benardop <benard.oc.p@gmail.com>
2023-03-22 23:28:00 +0300

    Add movie repository interface

--- src/main/java/com/benard/movies/MovieRepository.java
+++ src/main/java/com/benard/movies/MovieRepository.java
@@ -1,4 +1,9 @@
 package com.benard.movies;
 
-public interface MovieRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 }
