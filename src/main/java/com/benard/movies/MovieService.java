43483d1080673e9deae2b1ac0cc550a65ee6b585
benardop <benard.oc.p@gmail.com>
2023-03-22 23:32:44 +0300

    Add movie service class

--- src/main/java/com/benard/movies/MovieService.java
+++ src/main/java/com/benard/movies/MovieService.java
@@ -1,4 +1,15 @@
 package com.benard.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+      return movieRepository.findAll();
+    }
 }
