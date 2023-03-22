de043692adf22c5be5187aa62f402f67825098ee
benardop <benard.oc.p@gmail.com>
2023-03-22 23:26:49 +0300

    Add movie controller class with API end-points

--- src/main/java/com/benard/movies/MovieController.java
+++ src/main/java/com/benard/movies/MovieController.java
@@ -1,4 +1,21 @@
 package com.benard.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
 }
