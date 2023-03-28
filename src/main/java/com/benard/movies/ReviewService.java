c2b428ecb0accb33e3ede89790a5adf638c24670
benardop <benard.oc.p@gmail.com>
2023-03-28 17:50:54 +0300

    Add movie review service class

--- src/main/java/com/benard/movies/ReviewService.java
+++ src/main/java/com/benard/movies/ReviewService.java
@@ -1,4 +1,30 @@
 package com.benard.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableAutoConfiguration
 public class ReviewService {
+    @Autowired
+    private  ReviewRespository reviewRespository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRespository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
 }
