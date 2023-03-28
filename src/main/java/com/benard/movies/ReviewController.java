df01c02320f900206540f5214309038902d216fa
benardop <benard.oc.p@gmail.com>
2023-03-28 17:49:54 +0300

    Add movie review controller class

--- src/main/java/com/benard/movies/ReviewController.java
+++ src/main/java/com/benard/movies/ReviewController.java
@@ -1,4 +1,23 @@
 package com.benard.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping()
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payLoad) {
+        return new ResponseEntity<Review>(reviewService.createReview(payLoad.get("reviewBody"), payLoad.get("imdbId")), HttpStatus.OK);
+    }
 }
