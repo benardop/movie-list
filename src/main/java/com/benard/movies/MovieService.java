0b4cae0cc96dbe86fffc8ac40849a9c24c6da7ef
benardop <benard.oc.p@gmail.com>
2023-03-28 17:48:03 +0300

    Add movie service class to retrieve list of movies from database

--- src/main/java/com/benard/movies/MovieService.java
+++ src/main/java/com/benard/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.benard.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,5 +13,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
       return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return  movieRepository.findMovieByImdbId(imdbId);
     }
 }
